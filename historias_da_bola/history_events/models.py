a0cfcdcc84f579ebc3a8459a0a9442ce98247dd3
Rafael Caricio <rafael.jacinto@gmail.com>
2012-04-07 15:57:16 -0300

    Fonts, initial layout and django project.

---
+++ historias_da_bola/history_events/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from cms.models import CMSPlugin
+
+class EventType(models.Model):
+    image = models.ImageField(upload_to="/event_types")
+    title = models.CharField(max_length=128)
+
+    def __unicode__(self):
+        return self.title
+
+class HistoryEvent(CMSPlugin):
+    year = models.PositiveSmallIntegerField()
+    event_type = models.ForeignKey(EventType)
+    title = models.CharField(max_length=255)
+    subtitle = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return self.title
