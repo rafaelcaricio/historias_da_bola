aca045fed3c3080ed8add27200084beed55bc960
Rafael Caricio <rafael.jacinto@gmail.com>
2012-04-16 01:05:35 -0300

    Começo do twitter plugin.

--- historias_da_bola/twitter_plugin/static/twitter_plugin/js/twitter_plugin.js
+++ historias_da_bola/twitter_plugin/static/twitter_plugin/js/twitter_plugin.js
@@ -1,3 +1,76 @@
 $(function() {
 
+    $.widget("historiasdabola.TwitterPlugin", {
+
+        options: {
+            user: null,
+            term: null,
+            tweets: 4
+        },
+
+        twitterAPI: {
+            search: 'http://search.twitter.com/search.json?&q=globo.com&rpp=4&include_entities=1&callback=jsonp1333819876214',
+            user: 'http://api.twitter.com/1/statuses/user_timeline.json?screen_name=rafaelcaricio&count=4&include_entities=1&callback=jsonp1333819756736'
+        },
+
+        _create: function() {
+            this._createBasicMarkup();
+            this._pullTweets();
+        },
+
+        _createBasicMarkup: function() {
+            ;
+        },
+
+        _pullTweets: function() {
+            var url,
+                params = {
+                    include_entities: true,
+                    rpp: this.options.tweets
+                };
+
+            if (this.options.user) {
+                url = this.twitterAPI.user;
+                params.screen_name = this.options.user;
+            } else if (this.options.term) {
+                url = this.twitterAPI.search;
+                params.q = this.options.term;
+            } else {
+                throw 'TwitterPlugin: you need to pass a search term or username.';
+            }
+
+            $.ajax(url, {
+                jsonp: true,
+                data: params,
+                success: $.proxy(this._onTweetsArrive, this)
+            });
+        },
+
+        _onTweetsArrive: function(data) {
+            $.each(data, $.proxy(function(i, tweet) {
+                this.appendTweet(tweet);
+            }, this));
+        },
+
+        _hash: function() {
+            return '';
+        },
+
+        _mention: function() {
+            return '';
+        },
+
+        _link: function() {
+            return '';
+        },
+
+        _appendTweet: function(tweet) {
+            this.elements.tweets.append([
+                '',
+                ''
+            ].join(''));
+        }
+
+    });
+
 });
